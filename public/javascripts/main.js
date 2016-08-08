695e4be6d43f1f5e30db7d2444f174cef209194c
Austin <abarrett@biohitech.com>
2016-08-08 19:00:56 -0400

    Add page scrool effect, navbar active state, and favicon

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,9 +1,21 @@
 /**
  * Created by austin on 8/5/16.
  */
+(function () {
+    $('.page-scroll a').bind('click', function (event) {
+        removeActiveClass();
+        $(this).parent().addClass('active');
+        var $position = $($(this).attr('href')).offset().top;
+        $('html, body').stop().animate({
+            scrollTop: $position - $offset
+        }, 600);
+        event.preventDefault();
+    });
 
-$("nav-bar-item").click(function() {
-    $('html, body').animate({
-        scrollTop: $("#" + section_name).offset().top
-    }, 2000);
-});
+    var removeActiveClass = function () {
+        var a = document.getElementsByClassName('page-scroll');
+        for (var i = 0; i<a.length; i++){
+            $(a[i]).removeClass('active')
+        }
+    };
+})();
